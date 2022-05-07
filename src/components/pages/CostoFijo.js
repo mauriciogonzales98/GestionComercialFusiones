4af7c93c42058bd80eec5e243c5dac39371a270f
mauricio gonzales <mauri9825@gmail.com>
2022-05-07 17:34:23 -0300

    Comenzado formulario

--- src/components/pages/CostoFijo.js
+++ src/components/pages/CostoFijo.js
@@ -10,75 +10,39 @@
 
 import React from 'react';
 
-export default class AddBook extends React.Component {
+export default class CostoFijo extends React.Component {
 
     constructor() {
         super();
         this.state = {
-            bookname: '',
-            author: '',
+            luz: '',
+            gas: '',
+            agua: '',
 
         };
 
     }
 
-
-    updateInfo = (event) => {
-        let fieldName = event.target.name;
-        let fieldValue = event.target.value;
-        if (fieldName === 'bookname') {
-            this.setState({ bookname: fieldValue });
-        }
-        else if (fieldName === 'author') {
-            this.setState({ author: fieldValue });
-        }
-    };
-
-
-    addBook = (e) => {
-
-        let { bookname, author } = this.state;
-        fetch('localhost:3000/add_book', {
-            method: 'post',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                bookname: bookname,
-                author: author,
-
-            })
-        }).then(response => response.json()).then(data => {
-            window.alert(data)
-            //Do anything else like Toast etc.
-        })
-
-    }
-
     render() {
-
         return (
-
-
-
-            <div className="add_book">
+            <div className="">
 
                 <div>
-                    <label>Book Name</label>
-                    <input onChange={this.updateInfo} name="bookname" value={this.state.bookname} />
+                    <label>Luz</label>
+                    <input name="luz" value={this.state.luz} />
                 </div>
                 <div>
-                    <label >Author</label>
-                    <input onChange={this.updateInfo} name="author" value={this.state.author} />
+                    <label >Gas</label>
+                    <input name="gas" value={this.state.gas} />
+                </div>
+                <div>
+                    <label >Agua</label>
+                    <input name="agua" value={this.state.agua} />
                 </div>
 
-                <button onClick={this.addBook}>Add</button>
+                <button >Add</button>
 
             </div>
-
         )
-
     }
-
-
-
-
 }
