355366cfd4c464695f831059c646da83fd217bcc
mauricio gonzales <mauri9825@gmail.com>
2022-05-07 17:50:53 -0300

    Creada conexion a db

---
+++ src/db.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test",
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+connection.query('SELECT * FROM usuarios', function (error, results, fields) {
+    if (error)
+        throw error;
+
+    results.forEach(result => {
+        console.log(result);
+    });
+});
